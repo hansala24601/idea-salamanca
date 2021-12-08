3844ed47f25013de09e3e1094e17552386710c46
hansala24601 <Han@Maceta.local>
2021-12-08 02:47:56 +0100

    mi componente de barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
 
-        <img src={logo} className="App-logo" alt="logo" />
+      <NavBar/>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Hola mundo</h1>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          Aprendiendo con mi idea de tienda
-
-        </a>
-
-
-      </header>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <h1>
+            Barra de navegacion
+        </h1>
+    )
+}
+
+export default NavBar;
